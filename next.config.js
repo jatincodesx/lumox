b6d636d33fa1bf06109e383d71e80c599ae6438f
Jatin Vohra <you@example.com>
2025-10-17 12:32:48 +1100

    edit next.config.js for static hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }, // TEMP—remove later
+  output: 'export',              // builds a static /out folder
+  images: { unoptimized: true }, // needed if you used next/image
+  trailingSlash: true,           // optional, but nice on static hosts
 };
 module.exports = nextConfig;
